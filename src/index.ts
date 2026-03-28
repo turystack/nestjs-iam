e07cab3b360f3b7e78406d3afb5a21e08a947ac0
João Tury <jgtb313@gmail.com>
2026-03-28 18:13:19 -0300

    updates

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,10 @@ export type {
 } from '@/iam.types.js'
 
 export { AclService } from '@/acl/acl.service.js'
-export { IamTokenService } from '@/token/token.service.js'
 export { IamCryptoService } from '@/crypto/crypto.service.js'
 export { ACL } from '@/decorators/acl.decorator.js'
 export { Auth } from '@/decorators/auth.decorator.js'
 export { Profile } from '@/decorators/authenticated-profile.decorator.js'
 export { IamForbiddenException } from '@/exceptions/iam-forbidden.exception.js'
 export { IamUnauthorizedException } from '@/exceptions/iam-unauthorized.exception.js'
+export { IamTokenService } from '@/token/token.service.js'
